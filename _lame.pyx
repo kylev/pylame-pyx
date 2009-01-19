b0aed547940dca3f51e69ec85f33b004db157415
Kyle VanderBeek <kylev@kylev.com>
2009-01-19 12:33:25 -0800

    Major elements of interface defined.

--- _lame.pyx
+++ _lame.pyx
@@ -2,13 +2,62 @@
 #cdef extern char* PYLAME_VERSION
 
 cdef extern from "lame/lame.h":
+    int vbr_off
+    int vbr_rh
+    int vbr_abr
+    int vbr_mtrh
+    int vbr_default
+
+    # MPEG modes
+    int STEREO
+    int JOINT_STEREO
+    int MONO
+    int NOT_SET
+
+    # Padding types
+    int PAD_NO
+    int PAD_ALL
+    int PAD_ADJUST
+
+    int ABR_8
+    int ABR_320
+    int V9
+    int V8
+    int V7
+    int V6
+    int V5
+    int V4
+    int V3
+    int V2
+    int V1
+    int V0
+
     char *get_lame_version()
     char *get_lame_short_version()
     char *get_lame_very_short_version()
     char *get_psy_version()
     char *get_lame_url()
 
+    ctypedef struct lame_version_t:
+        int major
+        int minor
+        int alpha
+        int beta
+        int psy_major
+        int psy_minor
+        int psy_alpha
+        int psy_beta
+        char *features
 
+    void get_lame_version_numerical(lame_version_t*)
+
+    ctypedef struct lame_global_flags
+
+    lame_global_flags *lame_init()
+    int lame_set_num_channels(lame_global_flags *, int)
+    int lame_get_num_channels(lame_global_flags *)
+
+# Core compiled definitions
 __version__ = "0.0.1"
 VERSION = get_lame_version()
 VERSION_SHORT = get_lame_short_version()
@@ -16,10 +65,70 @@ VERSION_TINY = get_lame_very_short_version()
 VERSION_PSY = get_psy_version()
 URL = get_lame_url()
 
+MPEG_MODE_STEREO = STEREO
+MPEG_MODE_JOINT_STEREO = JOINT_STEREO
+MPEG_MODE_MONO = MONO
+MPEG_NOT_SET = NOT_SET
 
-class Encoder:
+PADDING_NO = PAD_NO
+PADDING_ALL = PAD_ALL
+PADDING_ADJUST = PAD_ADJUST
+
+# VBR settings
+VBR_MODE_OFF = vbr_off
+VBR_MODE_RH = vbr_rh
+VBR_MODE_ABR = vbr_abr
+VBR_MODE_MTRH = vbr_mtrh
+VBR_MODE_DEFAULT = vbr_default
+
+PRESET_ABR_8 = ABR_8
+PRESET_ABR_320 = ABR_320
+PRESET_VBR_9 = V9
+PRESET_VBR_8 = V8
+PRESET_VBR_7 = V7
+PRESET_VBR_6 = V6
+PRESET_VBR_5 = V5
+PRESET_VBR_4 = V4
+PRESET_VBR_3 = V3
+PRESET_VBR_2 = V2
+PRESET_VBR_1 = V1
+PRESET_VBR_0 = V0
+
+
+def version():
+    """Returns the version of LAME in a tuple: (major, minor, alpha, beta,
+    psy_major, sy_minor, psy_alpha, psy_beta, compile_time_features).
+    """
+    cdef lame_version_t version
+
+    get_lame_version_numerical(&version)
+
+    return (version.major,
+            version.minor,
+            version.alpha,
+            version.beta,
+            version.psy_major,
+            version.psy_minor,
+            version.psy_alpha,
+            version.psy_beta,
+            version.features)
+
+
+cdef class Encoder:
     """A LAME encoder object."""
-    def foo():
+    cdef lame_global_flags *flags
+
+    def __cinit__(self):
+        self.flags = lame_init()
+        if self.flags == NULL:
+            raise MemoryError("lame_init failed")
+
+    def set_num_channels(self, int value):
+        lame_set_num_channels(self.flags, value)
+
+    def get_num_channels(self):
+        return lame_get_num_channels(self.flags)
+
+    def foo(self):
         """asdf"""
         pass
-
--- lame.py
+++ lame.py
@@ -1,4 +1,19 @@
 from _lame import *
 
+__all__ = ['__version__', 'VERSION', 'VERSION_SHORT',
+           'VERSION_TINY', 'VERSION_PSY', 'URL',
+           'MPEG_MODE_STEREO', 'MPEG_MODE_JOINT_STEREO', 'MPEG_MODE_MONO',
+           'MPEG_NOT_SET',
+           'PADDING_NO', 'PADDING_ALL', 'PADDING_ADJUST',
+           'VBR_MODE_OFF',' VBR_MODE_RH', 'VBR_MODE_ABR', 'VBR_MODE_MTRH',
+           'VBR_MODE_DEFAULT',
+           'PRESET_ABR_8', 'PRESET_ABR_320', 'PRESET_VBR_9', 'PRESET_VBR_8',
+           'PRESET_VBR_7', 'PRESET_VBR_6', 'PRESET_VBR_5', 'PRESET_VBR_4',
+           'PRESET_VBR_3', 'PRESET_VBR_2', 'PRESET_VBR_1', 'PRESET_VBR_0',
+           'Encoder', 'version',
+           'encode_file']
+           
+
 def encode_file(in_file, out_file, encoder=None):
-    pass
+    """Encode a file."""
+    raise NotImplementedError("I'm workin' on it!")
