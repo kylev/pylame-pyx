ef92040a940b5e688d0ef72569c2cf00d8296b16
Kyle VanderBeek <kylev@kylev-lt.localdomain>
2009-01-19 00:33:26 -0800

    working global constants and basics, plus python wrapper (for real this time)

--- _lame.pyx
+++ _lame.pyx
@@ -1,9 +1,25 @@
+# TODO Get -D constant working
+#cdef extern char* PYLAME_VERSION
+
 cdef extern from "lame/lame.h":
+    char *get_lame_version()
+    char *get_lame_short_version()
+    char *get_lame_very_short_version()
+    char *get_psy_version()
     char *get_lame_url()
 
-def url():
-    return get_lame_url()
+
+__version__ = "0.0.1"
+VERSION = get_lame_version()
+VERSION_SHORT = get_lame_short_version()
+VERSION_TINY = get_lame_very_short_version()
+VERSION_PSY = get_psy_version()
+URL = get_lame_url()
 
 
 class Encoder:
-      pass
+    """A LAME encoder object."""
+    def foo():
+        """asdf"""
+        pass
+
--- setup.py
+++ setup.py
@@ -2,8 +2,18 @@ from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
 
+
+pylame_version = '0.0.1'
+pylame_ext = Extension("_lame", ["_lame.pyx"],
+                       libraries=['mp3lame'],
+                       define_macros=[('PYLAME_VERSION', pylame_version)])
 setup(
-    cmdclass = {'build_ext': build_ext},
-    ext_modules = [Extension("_lame", ["_lame.pyx"],
-                             libraries=['mp3lame'])]
+    name='lame',
+    version=pylame_version,
+    description='Bindings for the LAME audio encoding library',
+    author='Kyle VanderBeek',
+    author_email='kylev@kylev.com',
+    cmdclass={'build_ext': build_ext},
+    py_modules=['lame'],
+    ext_modules=[pylame_ext],
 )
